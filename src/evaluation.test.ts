9174cfa4460b4fea55bdba524edfc2617ca0162c
andy <40680668+ajsmth@users.noreply.github.com>
2022-05-21 11:06:22 -0700

    evaluation for conditionals, return statements

--- src/evaluation.test.ts
+++ src/evaluation.test.ts
@@ -9,79 +9,85 @@ describe('evaluation', () => {
       10;
     `;
 
-    const expected = [5, 10];
-
-    testEvaluation(input, expected);
+    expectToEqual('5', 5);
+    expectToEqual('10', 10);
   });
 
   test('booleans', () => {
-    const input = `
-      true;
-      false;
-      1 < 2;
-      1 > 2;
-      1 < 1;
-      1 > 1;
-      1 != 1;
-      1 == 1;
-      1 != 2;
-      1 == 2;
-    `;
-
-    const expected = [
-      true,
-      false,
-      true,
-      false,
-      false,
-      false,
-      false,
-      true,
-      true,
-      false,
-    ];
-
-    testEvaluation(input, expected);
+    expectToEqual('true', true);
+    expectToEqual('false', false);
+    expectToEqual('1 < 2', true);
+    expectToEqual('1 > 2', false);
+    expectToEqual('1 < 1', false);
+    expectToEqual('1 != 1', false);
+    expectToEqual('1 == 1', true);
+    expectToEqual('1 != 2', true);
+    expectToEqual('1 == 2', false);
   });
 
   test('infix', () => {
-    const input = `
-      5 * 5;
-      5 + 5;
-      5 - 5;
-      5 / 5;
-      5 * 2 + 10;
-      2 * 2 * 2;
-      (5 + 10) * 2;
-      50 / (2 + 3);
-  `;
-
-    const expected = [25, 10, 0, 1, 20, 8, 30, 10];
-
-    testEvaluation(input, expected);
+    expectToEqual('5 * 5', 25);
+    expectToEqual('5 + 5', 10);
+    expectToEqual('5 / 5', 1);
+    expectToEqual('5 - 5', 0);
+    expectToEqual('5 * 2 + 10', 20);
+    expectToEqual('(5 + 10) * 2', 30);
+    expectToEqual('50 / (2 + 3)', 10);
   });
 
   test('prefix', () => {
+    expectToEqual('!true', false);
+    expectToEqual('!false', true);
+    expectToEqual('!5', false);
+    expectToEqual('!!true', true);
+    expectToEqual('!!false', false);
+    expectToEqual('!!5', true);
+  });
+
+  test('conditionals', () => {
+    expectToEqual('if (true) { 10 };', 10);
+    expectToEqual('if (false) { 10 };', null);
+    expectToEqual('if (1) { 10 };', 10);
+    expectToEqual('if (1 > 2) { 10 };', null);
+    expectToEqual('if (1 < 2) { 10 };', 10);
+    expectToEqual('if (1 > 2 ) { 10 } else { 20 };', 20);
+    expectToEqual('if (1 < 2) { 10 } else { 20 };', 10);
+  });
+
+  test('return', () => {
     const input = `
-      !true;
-      !false;
-      !5;
-      !!true;
-      !!false;
-      !!5;
+      5 * 5 * 5;
+      return 10;
+      9 * 9 * 9;
     `;
 
-    const expected = [false, true, false, true, false, true];
+    expectToEqual(
+      `  
+      5 * 5 * 5;
+      return 10;
+      9 * 9 * 9;
+    `,
+      10
+    );
 
-    testEvaluation(input, expected);
+    expectToEqual(
+      `
+      if (10 > 1) {
+        if (10 > 1) {
+          return 10;
+        }
+
+        return 1;
+      }
+    `,
+      10
+    );
   });
 });
 
-function testEvaluation(input: string, expected: any[]) {
+function expectToEqual(input: string, expected: any) {
   const tokens = lexer(input);
   const ast = parser(tokens);
-
-  const output = ast.statements.map(evaluate);
-
+  const output = evaluate(ast.statements);
   expect(output).toEqual(expected);
 }
--- src/evaluation.ts
+++ src/evaluation.ts
@@ -1,58 +1,105 @@
-export function evaluate(statement: any) {
-  if (statement.type === 'integer-literal') {
-    return statement.value;
-  }
+export function evaluate(statements: any[]) {
+  let result;
 
-  if (statement.type === 'boolean-literal') {
-    return statement.value;
-  }
-
-  if (statement.type === 'infix-operator') {
-    const left = evaluate(statement.left);
-    const right = evaluate(statement.right);
-
-    if (statement.operator === '+') {
-      return left + right;
-    }
-
-    if (statement.operator === '-') {
-      return left - right;
-    }
-
-    if (statement.operator === '*') {
-      return left * right;
-    }
-
-    if (statement.operator === '/') {
-      return left / right;
-    }
-
-    if (statement.operator === '<') {
-      return left < right
-    }
-
-    if (statement.operator === '>') {
-      return left > right
-    }
-
-    if (statement.operator === '!=') {
-      return left != right
-    }
-
-    if (statement.operator === '==') {
-      return left == right
+  for (let statement of statements) {
+    result = evaluateStatement(statement);
+    
+    if (statement.type === 'return') {
+      return result;
     }
   }
 
-  if (statement.type === 'prefix-operator') {
-    const value = evaluate(statement.value);
+  return result;
 
-    if (statement.operator === '!') {
-      return !value;
+  function evaluateStatement(statement: any) {
+    if (statement.type === 'integer-literal') {
+      return statement.value;
     }
 
-    if (statement.operator === '-') {
-      return -value;
+    if (statement.type === 'boolean-literal') {
+      return statement.value;
     }
+
+    if (statement.type === 'infix-operator') {
+      const left = evaluateStatement(statement.left);
+      const right = evaluateStatement(statement.right);
+
+      if (statement.operator === '+') {
+        return left + right;
+      }
+
+      if (statement.operator === '-') {
+        return left - right;
+      }
+
+      if (statement.operator === '*') {
+        return left * right;
+      }
+
+      if (statement.operator === '/') {
+        return left / right;
+      }
+
+      if (statement.operator === '<') {
+        return left < right;
+      }
+
+      if (statement.operator === '>') {
+        return left > right;
+      }
+
+      if (statement.operator === '!=') {
+        return left != right;
+      }
+
+      if (statement.operator === '==') {
+        return left == right;
+      }
+    }
+
+    if (statement.type === 'prefix-operator') {
+      const value = evaluateStatement(statement.value);
+
+      if (statement.operator === '!') {
+        return !value;
+      }
+
+      if (statement.operator === '-') {
+        return -value;
+      }
+    }
+
+    if (statement.type === 'if') {
+      const condition = evaluateStatement(statement.condition);
+      if (Boolean(condition)) {
+        const csq = evaluateStatement(statement.consequence);
+        return csq
+      } else if (statement.alternative != null) {
+        return evaluateStatement(statement.alternative);
+      } else {
+        return null;
+      }
+    }
+
+    if (statement.type === 'block') {
+      return evaluateBlock(statement);
+    }
+
+    if (statement.type === 'return') {
+      return evaluateStatement(statement.value);
+    }
+  }
+
+  function evaluateBlock(blockStatement: any) {
+    let result;
+
+    for (let s of blockStatement.statements) {
+      result = evaluateStatement(s);
+
+      s.type === 'return';
+      return result;
+    }
+
+    return result;
   }
 }
